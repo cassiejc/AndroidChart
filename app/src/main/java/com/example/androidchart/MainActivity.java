99ba8d3a1af1ca4c4fdc383015615b5e6f8f4ede
cassie <cassiejc123@gmail.com>
2024-05-16 12:48:24 +0700

    Initial commit

---
+++ app/src/main/java/com/example/androidchart/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.androidchart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.github.mikephil.charting.charts.BarChart;
+
+public class MainActivity extends AppCompatActivity {
+    BarChart
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
